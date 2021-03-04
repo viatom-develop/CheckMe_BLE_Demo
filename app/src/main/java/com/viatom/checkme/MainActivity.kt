f403d794733711cb22beba433f18e2dae8acfdbe
李光浩 <531147618@qq.com>
2021-03-04 14:45:01 +0800

    n2 rocket

--- app/src/main/java/com/viatom/checkme/MainActivity.kt
+++ app/src/main/java/com/viatom/checkme/MainActivity.kt
@@ -15,11 +15,16 @@ import android.os.Bundle
 import android.os.Vibrator
 import android.provider.Settings
 import android.util.Log
+import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.WindowManager
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -36,17 +41,17 @@ import com.vaca.x1.utils.toUInt
 import com.viatom.checkme.adapter.BlePanelAdapter
 import com.viatom.checkme.adapter.BleViewAdapter
 import com.viatom.checkme.adapter.UserViewAdapter
+import com.viatom.checkme.bean.BleBean
+import com.viatom.checkme.bean.UserBean
 import com.viatom.checkme.ble.EndReadPkg
 import com.viatom.checkme.ble.FDAResponse
 import com.viatom.checkme.ble.ReadContentPkg
 import com.viatom.checkme.ble.StartReadPkg
 import com.viatom.checkme.utils.CRCUtils.calCRC8
-import com.viatom.checkme.bean.BleBean
-import com.viatom.checkme.bean.UserBean
+import com.viatom.checkme.viewmodel.LeftHead
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.right_drawer.*
 import kotlinx.android.synthetic.main.scan_view.*
-
 import me.weyye.hipermission.HiPermission
 import me.weyye.hipermission.PermissionCallback
 import me.weyye.hipermission.PermissionItem
@@ -67,7 +72,17 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
     lateinit var bleViewAdapter: BleViewAdapter
     private var cmdState=0;
     private var fileType=0;
-    var fileName=arrayOf("usr.dat","dlc.dat","spc.dat","bpcal.dat","ecg.dat","oxi.dat","tmp.dat","slm.dat","ped.dat")
+    var fileName=arrayOf(
+        "usr.dat",
+        "dlc.dat",
+        "spc.dat",
+        "bpcal.dat",
+        "ecg.dat",
+        "oxi.dat",
+        "tmp.dat",
+        "slm.dat",
+        "ped.dat"
+    )
     var userID:ByteArray?=null
     var currentFileIndex=0;
     lateinit var blePanelAdapter: BlePanelAdapter
@@ -75,11 +90,14 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
     var pkgTotal=0;
     var currentPkg=0;
     var fileData: ByteArray? = null
-
+    val model:LeftHead by viewModels()
     lateinit var userInfo:UserFile.UserInfo
+    lateinit var leftHeadIcon:ImageView
+    lateinit var leftName: TextView
+
 
     var filePath:String?=null
-    fun getPathX(): String? {
+    private fun getPathX(): String? {
         val fs = getExternalFilesDirs(null)
         var extPath = ""
         if (fs != null && fs.size >= 1) {
@@ -88,7 +106,7 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
         return extPath
     }
 
-    fun getPathX(s:String): String? {
+    private fun getPathX(s: String): String? {
         return filePath+s
     }
 
@@ -144,7 +162,16 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
         this.finish()
     }
 
-    fun initDrawer(){
+    private fun addLiveDateObserver(){
+        model.headIcon.observe(this, {
+            leftHeadIcon.setImageResource(Constant.ICO_IMG[it-1])
+        })
+        model.headName.observe(this,{
+            leftName.text=it
+        })
+    }
+
+    private fun initDrawer(){
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
@@ -152,13 +179,23 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_1, R.id.nav_2, R.id.nav_3), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_1, R.id.nav_2, R.id.nav_3
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        val headerLayout: View = navView.getHeaderView(0)
+        leftHeadIcon=headerLayout.findViewById(R.id.imageView)
+        leftName=headerLayout.findViewById(R.id.userName)
+        leftHeadIcon.setOnClickListener {
+            drawerLayout.closeDrawer(GravityCompat.START)
+            drawerLayout.openDrawer(GravityCompat.END)
+        }
     }
 
-    fun initActionBarColor(){
+    private fun initActionBarColor(){
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getWindow().setStatusBarColor(Color.BLACK);
     }
@@ -168,6 +205,7 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
         initActionBarColor()
         setContentView(R.layout.activity_main)
         initDrawer()
+        addLiveDateObserver()
         initVar()
         initView()
         AskPermission()
@@ -231,7 +269,7 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
         leScanner.startScan(null, settings, leScanCallback)
     }
 
-    fun initView(){
+    private fun initView(){
         ble_table.layoutManager= GridLayoutManager(this, 2);
         bleViewAdapter= BleViewAdapter(this)
         ble_table.adapter=bleViewAdapter
@@ -244,7 +282,7 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
 
         val linearLayoutManager=LinearLayoutManager(this)
         linearLayoutManager.orientation=RecyclerView.VERTICAL
-        userAdapter= UserViewAdapter(this,right_user)
+        userAdapter= UserViewAdapter(this, right_user)
         right_user.adapter=userAdapter
         right_user.layoutManager=linearLayoutManager
         userAdapter.setClickListener(this)
@@ -253,7 +291,7 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
 
     }
 
-    fun initVar(){
+    private fun initVar(){
         filePath=getPathX()
         myBleManager= FDABleManager(this)
         myBleManager.setNotifyListener(this)
@@ -320,24 +358,24 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
     }
 
 
-    fun hasResponse(bytes: ByteArray?): ByteArray? {
+    private fun hasResponse(bytes: ByteArray?): ByteArray? {
         val bytesLeft: ByteArray? = bytes
 
         if (bytes == null || bytes.size < 8) {
             return bytes
         }
         loop@ for (i in 0 until bytes.size-7) {
-            if (bytes[i] != 0x55.toByte() || bytes[i+1] != bytes[i+2].inv()) {
+            if (bytes[i] != 0x55.toByte() || bytes[i + 1] != bytes[i + 2].inv()) {
                 continue@loop
             }
 
             // need content length
-            val len = toUInt(bytes.copyOfRange(i+5, i+7))
+            val len = toUInt(bytes.copyOfRange(i + 5, i + 7))
             if (i+8+len > bytes.size) {
                 continue@loop
             }
 
-            val temp: ByteArray = bytes.copyOfRange(i, i+8+len)
+            val temp: ByteArray = bytes.copyOfRange(i, i + 8 + len)
             if (temp.last() == calCRC8(temp)) {
                 var s:String="   "
                 for(k in temp){
@@ -353,7 +391,7 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
                 val bleResponse= FDAResponse.CheckMeResponse(temp)
                 if(cmdState==1){
                     fileData=null
-                    Log.e("数量","顺利打开房间昆仑山但是大家考虑是否收到    ${toUInt(bleResponse.content)}")
+                    Log.e("数量", "顺利打开房间昆仑山但是大家考虑是否收到    ${toUInt(bleResponse.content)}")
                     pkgTotal= toUInt(bleResponse.content)/1024
                     if(bleResponse.cmd==1){
                         val pkg= EndReadPkg()
@@ -369,7 +407,7 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
 
                 }else if(cmdState==2){
                     bleResponse.content.apply {
-                        fileData=add(fileData,this)
+                        fileData=add(fileData, this)
                     }
 
                     if(currentPkg>pkgTotal){
@@ -384,12 +422,12 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
                                 sleep(300)
                                 userAdapter.setUser(0)
                                 runOnUiThread {
-                                    onUserItemClick(userAdapter.mUserData[0],0)
+                                    onUserItemClick(userAdapter.mUserData[0], 0)
                                 }
 
                             }.start()
 
-                            Log.e("数量222","顺利打开房间昆仑山但是大家考虑是否收到    ${this.size}     ${s.toString()}")
+
                             File(getPathX(fileName[currentFileIndex])).writeBytes(this)
                         }
                         val pkg=EndReadPkg()
@@ -407,7 +445,10 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
                     cmdState=0;
                     sum=0
                 }
-                val tempBytes: ByteArray? = if (i+8+len == bytes.size) null else bytes.copyOfRange(i+8+len, bytes.size)
+                val tempBytes: ByteArray? = if (i+8+len == bytes.size) null else bytes.copyOfRange(
+                    i + 8 + len,
+                    bytes.size
+                )
 
                 return hasResponse(tempBytes)
             }
@@ -424,13 +465,15 @@ class MainActivity : AppCompatActivity() , BleViewAdapter.ItemClickListener, Ble
 
     override fun onUserItemClick(userBean: UserBean?, position: Int) {
             userBean?.apply {
+                model.headIcon.value=ico
+                model.headName.value=name
                 rName.text=name
                 if(sex==0){
                     rSex.text="Male"
                 }else{
                     rSex.text="Female"
                 }
-                val dateFormat = SimpleDateFormat("MMM. d, yyyy",ENGLISH )
+                val dateFormat = SimpleDateFormat("MMM. d, yyyy", ENGLISH)
                 rBirthday.text=dateFormat.format(birthday)
                 rWeight.text=weight.toString()
                 rHeight.text=height.toString()
---
+++ app/src/main/java/com/viatom/checkme/viewmodel/LeftHead.kt
@@ -0,0 +1,14 @@
+package com.viatom.checkme.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LeftHead:ViewModel() {
+    val headIcon:MutableLiveData<Int> by lazy{
+        MutableLiveData<Int>()
+    }
+
+    val headName:MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+}
